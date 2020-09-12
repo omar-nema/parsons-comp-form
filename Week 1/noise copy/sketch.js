754f7e782255b433fc25f3172ba9673acab65f25
Omar Nema <omar.nema@arcadiasolutions.com>
2020-09-11 22:51:49 -0400

    metallic

---
+++ Week 1/noise copy/sketch.js
@@ -0,0 +1,57 @@
+var scl = 5;
+var cols, rows;
+let inc = 0.1;
+
+var zoff = 0;
+
+function setup() {
+    createCanvas(600, 600);
+    background(240)   
+
+    cols = floor(width / scl)
+    rows = floor(height / scl);
+    console.log(cols,rows)
+}
+
+
+function draw() {
+    background(240);
+    var yoff = 0;
+    //var xoff = 0;
+    zoff += .1;
+
+    randomSeed(10)
+   
+
+    for (let y = 0; y < rows; y ++ ){
+        var xoff = 0;
+        yoff += inc;
+        for (let x = 0; x < cols; x ++ ){
+            //yoff += inc;
+
+            push()
+            // strokeWeight(0.3);
+            col = color(random(255), random(255), random(255))
+            col.setAlpha(random(255))
+            stroke(col)
+            strokeWeight(10);
+            
+            var angle = noise(xoff, yoff, zoff) * TWO_PI;
+            var v = p5.Vector.fromAngle(angle);
+            translate(x*scl, y*scl)
+            rotate(v.heading());
+            line(0, 0, scl, 0)
+            pop()
+    
+            xoff += inc;
+        }
+    }
+
+
+   
+
+
+
+
+
+}
--- Week 1/noise/sketch.js
+++ Week 1/noise/sketch.js
@@ -5,7 +5,7 @@ let inc = 0.1;
 var zoff = 0;
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(700, 700);
     background(240)   
 
     cols = floor(width / scl)
@@ -13,47 +13,63 @@ function setup() {
     console.log(cols,rows)
 }
 
-
+var boff = 0;
 function draw() {
-    background(240);
+     background('#8afff7');
+  
+    boff += 1; 
+   
     var yoff = 0;
     //var xoff = 0;
-    zoff += .1;
+    zoff += .03;
 
-    noLoop();
 
-    for (i=0; i < 5; i++){
-        for (let y = 0; y < rows; y ++ ){
-            var xoff = 0;
-            yoff += inc;
-            for (let x = 0; x < cols; x ++ ){
-                //yoff += inc;
+
+    randomSeed(10)
+   
+
+    for (let y = 0; y < rows; y ++ ){
+        var xoff = 0;
+        yoff += inc;
+
+        
+        for (let x = 0; x < cols; x ++ ){
+            //yoff += inc;
+
+            push()
+            // strokeWeight(0.3);
+            // col = color(random(255), random(255), random(255))
+            // col.setAlpha(random(255))
+            // stroke(col)
+            strokeWeight(1);
+            stroke('black')
+            var angle = noise(xoff, yoff, zoff) * TWO_PI;
+            var v = p5.Vector.fromAngle(angle);
+            translate(x*scl, y*scl)
+            rotate(v.heading());
+            line(0, 0, scl, 0)
+
+
+            
+            noStroke();
+            strokeWeight(1)
+            //stroke(noise(xoff, yoff)*255)
+            ///fill(random(255))
+            fill(20)
+        
+            
+            rect(0,0,scl,scl);
+            
+
+            
+            pop()
+
+            
     
-                push()
-                // strokeWeight(0.3);
-                col = color(random(255), random(255), random(255))
-                col.setAlpha(random(255))
-                stroke(col)
-                strokeWeight(10);
-                
-                var angle = noise(xoff, yoff, zoff) * TWO_PI;
-                var v = p5.Vector.fromAngle(angle);
-                translate(x*scl, y*scl)
-                rotate(v.heading());
-                line(0, 0, scl, 0)
-                pop()
-                // push()
-                // stroke('rgba(0,0,0,.2)')
-                // var angle = noise(xoff+1000, yoff+10000, zoff) * PI;
-                // var v = p5.Vector.fromAngle(angle);
-                // translate(x*scl, y*scl)
-                // rotate(v.heading());
-                // line(0, 0, scl, 0)
-                // pop()
-                xoff += inc;
-            }
+            xoff += inc;
         }
     }
+
 
    
 
