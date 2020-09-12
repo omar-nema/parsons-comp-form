81f9444a83daa0827f967ba6a36dfd9567947c2c
Omar Nema <omar.nema@arcadiasolutions.com>
2020-09-11 20:52:57 -0400

    i like this one

---
+++ Week 1/noise/particle.js
@@ -0,0 +1,10 @@
+// function Particle() {
+//     this.pos = createVector(0,0);
+//     this.vel = createVector(0,0);
+//     this.acc = createVector(0,0);
+// }
+
+
+// this.update = function() {
+//     this.
+// }
--- Week 1/noise/sketch.js
+++ Week 1/noise/sketch.js
@@ -1,11 +1,11 @@
-var scl = 10;
+var scl = 5;
 var cols, rows;
 let inc = 0.1;
 
 var zoff = 0;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
     background(240)   
 
     cols = floor(width / scl)
@@ -16,32 +16,47 @@ function setup() {
 
 function draw() {
     background(240);
-    noLoop();
-
-  
     var yoff = 0;
     //var xoff = 0;
-    for (let y = 0; y < rows; y ++ ){
-        var xoff = 0;
-        yoff += inc;
-        for (let x = 0; x < cols; x ++ ){
-            //yoff += inc;
+    zoff += .1;
 
-            push()
-            var angle = noise(xoff, yoff) * TWO_PI;
+    noLoop();
 
-            var v = p5.Vector.fromAngle(angle);
-            translate(x*scl, y*scl)
-            rotate(v.heading());
-            line(0, 0, scl, 0)
-            pop()
-
-            xoff += inc;
+    for (i=0; i < 5; i++){
+        for (let y = 0; y < rows; y ++ ){
+            var xoff = 0;
+            yoff += inc;
+            for (let x = 0; x < cols; x ++ ){
+                //yoff += inc;
+    
+                push()
+                // strokeWeight(0.3);
+                col = color(random(255), random(255), random(255))
+                col.setAlpha(random(255))
+                stroke(col)
+                strokeWeight(10);
+                
+                var angle = noise(xoff, yoff, zoff) * TWO_PI;
+                var v = p5.Vector.fromAngle(angle);
+                translate(x*scl, y*scl)
+                rotate(v.heading());
+                line(0, 0, scl, 0)
+                pop()
+                // push()
+                // stroke('rgba(0,0,0,.2)')
+                // var angle = noise(xoff+1000, yoff+10000, zoff) * PI;
+                // var v = p5.Vector.fromAngle(angle);
+                // translate(x*scl, y*scl)
+                // rotate(v.heading());
+                // line(0, 0, scl, 0)
+                // pop()
+                xoff += inc;
+            }
         }
-       
     }
 
- 
+   
+
 
 
 
