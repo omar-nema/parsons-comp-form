5067adbf3b37a149b4fa461ef302253506e4a01d
Omar Nema <omar.nema@arcadiasolutions.com>
2020-09-24 08:27:20 -0400

    noise

--- Week 4/mapattempt2/sketch.js
+++ Week 4/mapattempt2/sketch.js
@@ -1,24 +1,30 @@
-let centerX;
-let centerY;
 
-let radius = 100;
 let numPoints = 100;
-
-let noiseAmount = 20;
+let noiseAmount = 7;
+var shapes = [];
+let treasureX, treasureY;
 
 function setup() {
   createCanvas(500, 500);
-    
   angleMode(DEGREES);
-  noStroke();
+  drawingContext.shadowOffsetX = 1;
+  drawingContext.shadowOffsetY = 2;
+  drawingContext.shadowBlur = 1;
+  drawingContext.shadowColor = 'black';
+  background('#0ecccc');
+
+  generateBlobs();
+  generateTreasure();
+  cursor(CROSS)
 }
 
-function drawBlob(centerX, centerY, shapeFactor){
+function drawBlob(centerX, centerY, shapeFactor, noiseFactor, radius){
     var negFactor = 1;
-    
 
+    maxRadius = radius;
+    radius = 0.8*radius;
+    
     pts = [];
-    greenpts = [];
     for (let i = 0; i < numPoints; i++) {
       let angle = (360 / numPoints) * i;
 
@@ -28,73 +34,118 @@ function drawBlob(centerX, centerY, shapeFactor){
    
       if (i % 5 == 0){
           if (negFactor == 1){
-              radius = radius*random(0.8, 1);
+              radius -= radius*(random(0, .2));
+              radius = min(maxRadius, radius);
               //radius = radius + random(radius/20)
           } else {
-              radius = radius*random(1, 1.2);
+            radius += radius*(random(0, .2));
+            radius = min(maxRadius, radius);
               //radius = radius + random(radius/20)
           }
       
       }
   
-      let x = cos(angle) * shapeFactor*radius + centerX;
+      let x = cos(angle) * radius*shapeFactor + centerX;
       let y = sin(angle) * radius + centerY;
+
       //rotate(PI / random(20, 100))
   
-      let xnoise = (noise(frameCount * 0.02 + i) - 0.5) * noiseAmount;
-      let ynoise = (noise(frameCount * 0.02 + i) - 0.5) * noiseAmount;
+      let xnoise = (noise(noiseFactor * 0.02 + i) - 0.5) * noiseAmount;
+      let ynoise = (noise(noiseFactor * 0.02 + i) - 0.5) * noiseAmount;
+      noiseFactor += 0.1;
       newPt = [x+xnoise, y+ynoise];
       pts.push(newPt)
 
-      greenPt = [newPt[0]*0.9, newPt[1]*0.9];
-      greenpts.push(greenPt);
-  
-      //curveVertex(x + xnoise, y + ynoise);
+        // greenpts = [];
+        //   greenPt = [newPt[0]*0.9, newPt[1]*0.9];
+        //   greenpts.push(greenPt);
+        //curveVertex(x + xnoise, y + ynoise);
     }
-
     push();
     beginShape();
-    fill(230);
+    strokeWeight(2);
+    noStroke()
+    fill('#ffc369');
     pts.forEach(function(d){
         curveVertex(d[0], d[1])
     })
     endShape(CLOSE);
+    rotate(random(-140, 140))
     pop();
-
   
 }
 
 
+function generateBlobs(){
+    let numIslands = 100;
+
+    //one initial big blob
+    let noiseFactor = 1;
+    rad = 220;
+    centerX = width * random(0.1, 0.9);
+    centerY = height * random(0.1, 0.9);
+    let blob = {
+        centerX: centerX,
+        centerY: centerY,
+        shapeFactor: random(0.5, 1),
+        noiseFactor: random(0.5, 1),
+        rad: rad
+    }
+    shapes.push(blob)
+    drawBlob(blob.centerX, blob.centerY, blob.shapeFactor, blob.noiseFactor, blob.rad)
+    noiseFactor += 100;
+
+    for (i=0; i< numIslands; i++){
+        shapeFactor = random(0.2, 1);
+        centerX = width * random(0.1, .9);
+        centerY = height * random(0.1, .9);
+        rad = random(10, 100)
+
+        var overlapping = false;
+        shapes.forEach(function(d){
+            dis = dist(d.centerX, d.centerY, centerX, centerY);
+            if ((d.rad + rad) > dis){
+                overlapping = true;
+            }
+        })
+        if (!overlapping){
+            blob = {
+                centerX: centerX,
+                centerY: centerY,
+                shapeFactor: random(0.5, 1.5),
+                noiseFactor: random(0.5, 1),
+                rad: rad
+            }
+            shapes.push(blob)
+            drawBlob(blob.centerX, blob.centerY, blob.shapeFactor, blob.noiseFactor, blob.rad)
+            noiseFactor += 1000;
+        }     
+    }
+}
+
+function generateTreasure(){
+    treasureIsland = shapes[0];
+    treasureRad = treasureIsland.rad*0.3;
+    treasureX = treasureIsland.centerX+random(-treasureRad, treasureRad);
+    treasureY = treasureIsland.centerY+random(-treasureRad, treasureRad);
+}
+
+
 function draw() {
-  background('#6ea2f5');
-  noLoop();
+    
+    //add treaure
 
-
-
-  
-    let shapeFactor = random(0.2, 1.5);
-
-    //feed diff noise offsets
-    centerX = width * random(0, 1);
-    centerY = height * random(0, 1);
-    drawBlob(centerX, centerY, shapeFactor);
-
-    // centerX = width * random(0, 1);
-    // centerY = height * random(0, 1);
-    // drawBlob(centerX, centerY, shapeFactor);
-
-    // centerX = width * random(0, 1);
-    // centerY = height * random(0, 1);
-    // drawBlob(centerX, centerY, shapeFactor);
-
-    // centerX = width * random(0, 1);
-    // centerY = height * random(0, 1);
-    // drawBlob(centerX, centerY, shapeFactor);
-
-
-   
-
-
-
+    push();
+    fill('red')
+    drawingContext.shadowOffsetX = 0.2;
+    drawingContext.shadowOffsetY = 0.2;
+    drawingContext.shadowBlur = 1;
+    drawingContext.shadowColor = 'rgba(0,0,0,0.2)';
+   // console.log(dist(mouseX, mouseY, treasureX, treasureY))
+    if (dist(mouseX, mouseY, treasureX, treasureY) < 50){
+        strokeWeight(0.5)
+        circle(treasureX, treasureY,10)
+    }
+    pop();
 
 }
