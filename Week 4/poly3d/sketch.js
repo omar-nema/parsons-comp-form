59952812d0428d9996b88705fafb35b9ab12fb43
Omar Nema <omar.nema@arcadiasolutions.com>
2020-09-26 11:18:32 -0400

    polygonitos

---
+++ Week 4/poly3d/sketch.js
@@ -0,0 +1,113 @@
+function preload() {
+
+}
+
+let rows = 10;
+let cols = rows;
+
+let unitHeight;
+let unitWidth;
+let shapeModes;
+
+function setup() {
+    createCanvas(700, 600)   
+    background(255)
+    unitHeight = height/rows;
+    unitWidth = width/cols;
+
+    shapeModes = [TRIANGLE_STRIP,  QUAD_STRIP, LINES, TRIANGLES]
+
+    // drawingContext.shadowOffsetX = 1;
+    // drawingContext.shadowOffsetY = -1;
+    // drawingContext.shadowBlur = 5;
+    // drawingContext.shadowColor = 'rgba(90,90,90, 0.4)';
+}
+
+
+colr = [1,4, 3, 9]
+function getColor(){
+    // colorMode(HSB, 100);
+    // colBase = parseInt(random(45, 60));
+    // console.log(colBase)
+    // return color(colBase, 100, 90);
+    return random(255);
+}
+
+
+function draw() {
+    noLoop();
+    randomSeed(5)
+    overlapfactor = 40;
+    
+    for (x=0; x < cols; x++){
+        minX = unitWidth*x - unitWidth/overlapfactor;
+        maxX = minX + unitWidth + unitWidth/overlapfactor;
+
+        for (y=0; y< rows; y++){
+            minY = unitHeight*y - unitHeight/overlapfactor;
+            maxY = minY + unitHeight + unitHeight/overlapfactor;
+        
+            push()
+            strokeWeight(1)
+            fill(getColor());
+            createShape(minX, minY, maxX, maxY, random(3, 7), .97)
+           
+            pop();
+
+
+            // push();
+            // fill(getColor())
+            // strokeWeight(1)
+            // polygon(random(minX, maxX), random(minY, maxY), random(3, 10), random(3,12))
+            // polygon(random(minX, maxX), random(minY, maxY), random(3, 9), random(1,5))
+            // pop();
+            //createShape(minX, minY, maxX, maxY, random(3, 7), .97)
+
+            //rand effects
+            // push()
+            // shearX(PI / random(3, 5))
+            // shearY(PI / random(3, 5))
+            // pop()       
+            
+            // polygon(random(currX, maxX), random(currY, maxY), random(2, 15), random(3,8))
+            // polygon(random(currX, maxX), random(currY, maxY), random(1, 3), random(3,8))
+            // polygon(random(currX, maxX), random(currY, maxY), random(1, 3), random(3,8))
+            // polygon(random(currX, maxX), random(currY, maxY), random(1, 3), random(3,8))
+            //polygon(random(currX, maxX), random(currY, maxY), random(5, 90), random(3,8))
+        }
+    }
+
+}
+
+
+
+function createShape(minX, minY, maxX, maxY, numpts){    
+
+    beginShape();
+    //beginShape(QUADS)
+    for (i=0; i< numpts; i++){
+        vertex(random(minX, maxX), random(minY, maxY))
+    }
+    endShape(CLOSE)
+}
+
+function createCurvedShape(minX, minY, maxX, maxY, numpts){    
+    beginShape();
+    curveTightness(random(1,5));
+    for (i=0; i< numpts; i++){
+        curveVertex(random(minX, maxX), random(minY, maxY))
+    }
+    endShape(CLOSE)
+}
+
+//regular polygon code from: https://p5js.org/examples/form-regular-polygon.html
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
---
+++ Week 4/terrainthing/sketch.js
@@ -0,0 +1,72 @@
+
+var cols = 50;
+var rows = cols;
+var unitWidth, unitHeight;
+
+var terrain = [];
+
+function setup() {
+    createCanvas(700, 700, WEBGL)  
+    noFill();
+    //create 2d array
+    for (var x=0; x<cols; x++){
+        terrain[x] = [];
+        for (var y=0; y < rows; y++){  
+            terrain[x][y] = 0;
+        }
+    }
+
+}
+
+var noiseRange = 150;
+var noiseInc = 0.2;
+var yGlobal = 0;
+
+function draw() {
+  
+    background(10); 
+    stroke('white')
+    strokeWeight(0.5);
+    noLoop();
+
+    yGlobal -= 0.05;
+    var yoff = yGlobal;
+    var xoff = 0;
+    for (var y=0; y < rows; y++){  
+        
+        for (var x=0; x<cols; x++){
+            terrain[x][y] = map(noise(xoff, yoff), 0, 1, 0, noiseRange);
+            xoff += noiseInc;
+        }
+        yoff += noiseInc;   
+    }
+   
+    unitWidth = width / cols;
+    unitHeight = height/rows;
+    // rotateX(PI / 30);
+    //rotateAmt += 0.01;
+ 
+    translate(-width/2, -height/4, 0);
+
+    // fill(
+    //     map(terrain[x][y], 0, 1, 0, 50),
+    //     map(terrain[x][y], 0, 1, 0, 200),
+    //     map(terrain[x][y], 0, 1, 0, 255)
+    //   );
+    stroke(150)
+    noStroke();
+    fillNum = 50;
+    for (var y=0; y<rows; y++){
+        beginShape(TRIANGLE_STRIP)
+        // fill(0,0,255, fillNum)
+        fill(random(255))
+        fillNum+= 4;
+        for (var x=0; x < cols; x++){  
+            vertex(x*unitWidth, y*unitHeight, terrain[x][y]);
+            vertex(x*unitWidth, (y+1)*unitHeight, terrain[x][y+1]);
+
+        }
+        endShape();
+    }
+
+}
