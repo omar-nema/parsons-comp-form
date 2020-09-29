276aa8cc4f891df2adb3dec7dced2be1ef45397a
Omar Nema <omar.nema@arcadiasolutions.com>
2020-09-28 20:04:30 -0400

    animation works but slow

--- Week 4/recursivetreenoisy/sketch.js
+++ Week 4/recursivetreenoisy/sketch.js
@@ -1,9 +1,3 @@
-
-
-//noiseness in limb length, stroke weight, color, etc etc
-
-
-
 function setup() {
     createCanvas(700, 700)   
     background(240);
@@ -11,28 +5,33 @@ function setup() {
 }
 
 
-var maxHt = 130;
+var maxHt = 100;
 var xoff = 0;
 var strokeCap = 5;
-var recurse = 0.73;
-var minHt = 2;
+var recurse = 0.6;
+var minHt = 1;
 var numRecursions = Math.log(minHt/maxHt)/Math.log(recurse)
 var maxStroke = 1   ;
 var strokeGradation = 7/(numRecursions) * 2;
-var angle = 15; 
-
 var bigAngle = 25;
-
 var numBranches = 1;
 
-function generateCurveVertices(numVertices, range, htVal){
+var noiseRange = 5;
+
+
+var branchPts = [];
+var noiseoff = 0;
+var runNum;
+var maxRotation = 10;
+
+function generateCurveVertices(numVertices, range, htVal, index){
     inc = htVal/numVertices; 
     currVal = 0;
     vertices = [];
     for (i=0; i<numVertices+1; i++){
-        // console.log(currVal);
+
         vertices.push({
-            x: random(-range,range),
+            x: map(noise((index+1)*(i+1)), 0, 1, -range, range),
             y: currVal
         })
         currVal += inc;
@@ -42,81 +41,77 @@ function generateCurveVertices(numVertices, range, htVal){
 
 function draw() {
   
-    noLoop();
-    background(240);
-    
-    strokeWeight(2);
-    translate(width/2, height/2)
+    // noLoop();
+    runNum = 0;
+    noiseoff += 0.1;
 
+    strokeWeight(1);
+    background(250);
+    
+    translate(width/2, height/2)
 
     translate(0,  maxHt)
 
+    //initial line
     beginShape();
-    curveVertex(0,0);
-    curveVertex(random(-5, 5),maxHt/2)
-    curveVertex(0,maxHt)
+    vertices = generateCurveVertices(4, noiseRange, maxHt, runNum);
+    vertices.forEach(function(d, i){
+        curveVertex(d.x, d.y)
+    })   
     endShape(CLOSE);
+    runNum ++;
     
-   
+    //recursion
     angleInc = 360/numBranches;
     currAngle = 0;
     for (i=0; i<numBranches; i++){
         push ()
-        branch(maxHt, angle, maxStroke) 
+        branch(maxHt, maxStroke) 
         pop ()
         currAngle += angleInc;
     }
   
 }
 
-var branchPts = [];
-var noiseoff = 0;
 
-function branch(ht, angle, strokeVal){
+
+function branch(ht, strokeVal){
     ht *= recurse;
-    noiseoff += 0.5;
-    strokeWeight(2)
+    runNum ++;
     if (ht > minHt){
 
-        angle = map(noise(noiseoff), 0, 1, bigAngle*0.5, bigAngle*2)
-        ht = map(noise(noiseoff), 0, 1, ht*0.95, ht*1.05)
-        
+        // angle = map(noise(noiseoff), 0, 1, bigAngle*0.5, bigAngle*2)
+        // ht = map(noise(noiseoff), 0, 1, ht*0.9, ht*1.1)
+
+        angle = bigAngle;
+        stroke (0, 200)
         push ()
         rotate (angle)
+        
         beginShape()
-        vertices = generateCurveVertices(4, 4, -ht);
+        rotate(map(noise(noiseoff), 0, 1, 0, maxRotation))
+        vertices = generateCurveVertices(4, noiseRange, -ht, runNum);
         vertices.forEach(function(d, i){
             curveVertex(d.x, d.y)
         })
         endShape(CLOSE)
-        
-        // beginShape()
-        // curveVertex(0,0)
-        // curveVertex(random(-2,2), -ht*.33)
-        // curveVertex(random(-2,2), -ht*.5)
-        // curveVertex(random(-2,2), -ht*.66)
-        // curveVertex(0, -ht)
-        // endShape(CLOSE)
-        // line(0,0, 0, -ht)
-
-
         translate(0, -ht)
         strokeVal -= strokeGradation;
-        branch(ht, angle, strokeVal)
+        branch(ht, strokeVal)
         pop ()
 
         push ()
         rotate (-angle)
-        // line(0,0, 0, -ht)
         beginShape()
-        curveVertex(0,0)
-        curveVertex(random(-2,2), -ht*.33)
-        curveVertex(random(-2,-2), -ht*.66)
-        curveVertex(0, -ht)
+        rotate(map(noise(noiseoff+5000), 0, 1, 0, maxRotation))
+        vertices = generateCurveVertices(4, noiseRange, -ht, runNum);
+        vertices.forEach(function(d, i){
+            curveVertex(d.x, d.y)
+        })
         endShape(CLOSE)
         translate(0, -ht)
         strokeVal -= strokeGradation;
-        branch(ht, angle, strokeVal)
+        branch(ht, strokeVal)
         pop ()
        
     } 
