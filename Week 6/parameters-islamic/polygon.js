f8a0e92f7e47baa1d578665c43422663fb22c947
Omar Nema <omar.nema@arcadiasolutions.com>
2020-10-12 10:53:12 -0400

    working

--- Week 6/parameters-islamic/polygon.js
+++ Week 6/parameters-islamic/polygon.js
@@ -15,9 +15,35 @@ function Polygon(){
     }
 
     this.show = function(){
+
+        // beginShape();
+        // noFill()
+        // stroke('red')
+        // for (var i=0; i< this.vertices.length; i++){
+        //     v = this.vertices[i];
+        //     vertex (v.x, v.y)
+        // }   
+        // endShape();
+
+       
         for (var i=0; i< this.edges.length; i++){
-            this.edges[i].show();
+            // this.edges[i].show();
+            var e = this.edges[i]
+            noFill()
+            // fill ('blue')
+            // vertex(e.a.x, e.a.y);
+            // vertex(e.b.x, e.b.y);
+            // vertex(e.offset1.x, e.offset1.y);
+            // vertex(e.end.x, e.end.y);
+            // vertex(e.offset2.x, e.offset2.y);
+            // vertex(e.end2.x, e.end2.y);
+       
+            line (e.a.x, e.a.y, e.b.x, e.b.y);
+            line (e.offset1.x, e.offset1.y, e.end.x, e.end.y)
+            line (e.offset2.x, e.offset2.y, e.end2.x, e.end2.y)
+
         }
+      
     }
     this.close = function(){
         var lastEdge = this.vertices[this.vertices.length -1];
@@ -29,33 +55,28 @@ function Polygon(){
 
     this.hankin = function(){
         for (i=0; i<this.edges.length;i++){
-            this.edges[i].hankin();
+            this.edges[i].hankin(this.edges.length);
         }
-        for (i=0; i<this.edges.length;i++){
-            for (j=0; j<this.edges.length;j++){
-                if (i !== j){
-                    this.edges[i].findEnds(this.edges[j])
-                }
-            } 
-        }
-   
     }
 
 }
 
 function Edge(a,b){
 
-    
     this.a = a;
     this.b = b;
     this.h1;
     this.h2;
+    this.offset1;
+    this.offset2;
+    this.end, this.end2;
 
-    this.hankin = function(a, b){
+    this.hankin = function(sides){
         //midpoint, and vectors from midpoint to edge
         var mid = p5.Vector.add(this.a, this.b)
         mid.mult(0.5);
         var v1 = p5.Vector.sub(this.a, mid);
+        var elen = v1.mag() + paramDelta;
         var v2 = p5.Vector.sub(this.b, mid);
         var offset1 = mid;
         var offset2 = mid;
@@ -71,24 +92,51 @@ function Edge(a,b){
         v1.rotate(-paramAngle)
         v2.rotate(paramAngle)
 
-      
-        this.h1 = new Hankin(offset1, v1);
-        this.h2 = new Hankin(offset2, v2);
+        var polygonAngle = ((sides - 2)*180) / sides;
+        var edgeAngle = 180 - polygonAngle*.5 - paramAngle;
+        hlen = (elen * sin(polygonAngle*.5))/sin(edgeAngle);
+
+        v1.setMag(hlen);
+        v2.setMag(hlen);
+
+        this.offset1 = offset1;
+        this.offset2 = offset2;
+        this.end = p5.Vector.add(offset1, v1);
+        this.end2 = p5.Vector.add(offset2, v2);
+
     }
 
-    this.findEnds = function(edge){
-        //this h1 = edge.h1, or this.h1 = edge.h2
-        //go through each edge. store distance along w/ output. return smallest
-        this.h1.findEnd(edge.h1);
-        this.h1.findEnd(edge.h2)
-        this.h2.findEnd(edge.h1)
-        this.h2.findEnd(edge.h2);
-    }
-
-    this.show = function(){
-       
-        line (this.a.x, this.a.y, this.b.x, this.b.y);
-        this.h1.show()
-        this.h2.show()
-    }
+    // this.show = function(){
+    //     stroke('red')
+    //     fill ('white')
+    //     line (this.a.x, this.a.y, this.b.x, this.b.y);
+    //     line (this.offset1.x, this.offset1.y, this.end.x, this.end.y)
+    //     line (this.offset2.x, this.offset2.y, this.end2.x, this.end2.y)
+    // }
 }
+
+
+
+
+
+
+      
+        // this.h1 = new Hankin(offset1, v1);
+        // this.h2 = new Hankin(offset2, v2);
+
+
+       // this.h1.show()
+        // this.h2.show()
+
+// function Hankin(a, v){
+
+//     this.a = a;
+//     this.end = p5.Vector.add(a, v)
+
+//     this.show = function(){
+//         stroke (255);
+//         fill (255)
+//         line (this.a.x, this.a.y, this.end.x, this.end.y)
+//     }
+    
+// }
--- Week 6/parameters-islamic/sketch.js
+++ Week 6/parameters-islamic/sketch.js
@@ -1,7 +1,8 @@
 var poly;
-var paramAngle, paramDelta;
+var paramAngle, paramDelta, paramRadius, paramShape, paramColor, paramScale;
 
-var sliderAngle, sliderDelta;
+var sliderAngle, sliderDelta, sliderRadius, sliderShape, sliderColor, sliderScale;
+var btnSubmit;
 
 function setup() {
     createCanvas(500, 500)   
@@ -12,37 +13,77 @@ function setup() {
     stroke (255)
 
     createP ('Angle')
-    sliderAngle = createSlider(0, 180, 60, 10);
+    sliderAngle = createSlider(0, 90, 30, 10);
 
     createP ('Center Offset')
-    sliderDelta = createSlider(0, 50, 10, 10);
-   
+    sliderDelta = createSlider(0, 20, 0, 1);
 
+    createP ('Size')
+    sliderRadius = createSlider(0, 250, 10, 1); 
+
+    createP ('Shape')
+    sliderShape = createSlider(3, 30, 4, 1); 
+
+    createP ('Color')
+    sliderColor = createColorPicker('#ffffff'); 
+
+    createP ('Scale')
+    sliderScale = createSlider(1, 50, 10, 1);    
+
+    // btnSubmit = createButton('Submit')
+    // btnSubmit.mousePressed(redraw)
 }
 
 function updateParams(){
     paramAngle = sliderAngle.value();
     paramDelta = sliderDelta.value();
-    //test commit
+    paramShape = sliderShape.value();
+    paramRadius = sliderRadius.value();
+    paramScale = sliderScale.value();    
+    paramColor = sliderColor.value();
 }
 
-// function createPolygon(){
 
-// }
+
+function createPolygon(x, y, radius, npoints) {
+    let angle = 360 / npoints;
+    poly = new Polygon();
+    for (let a = 0; a < 360; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      poly.addVertex(sx, sy);
+    }
+    poly.close();
+    poly.hankin();
+    poly.show();
+  }
+
 
 function draw() {
 
-    background(0);
+    background(40);
     // noLoop();
     updateParams();
-    poly = new Polygon();
-    poly.addVertex(100, 100);
-    poly.addVertex(300, 100);
-    poly.addVertex(300, 300);
-    poly.addVertex(100,300);
-    poly.close()
-    poly.hankin();
-    poly.show();
+    stroke(paramColor);
+    strokeWeight(1)
+
+    //createPolygon(width/2,height/2, paramRadius, paramShape)
+    let unitSize = width/paramScale; 
+    for (var y=0; y<paramScale; y++){
+        for (var x=0; x<paramScale; x++){
+            createPolygon(x*(unitSize+2), y*(unitSize+2), paramRadius, paramShape);
+        }
+    }
+
+
+    // poly = new Polygon();
+    // poly.addVertex(100, 100);
+    // poly.addVertex(300, 100);
+    // poly.addVertex(300, 300);
+    // poly.addVertex(100,300);
+    // poly.close()
+    // poly.hankin();
+    // poly.show();
 
 }
 
