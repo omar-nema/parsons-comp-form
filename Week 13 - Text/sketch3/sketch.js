997393b57f134fe6258ff8813783dcc0a538fb00
Omar Nema <omar.nema@arcadiasolutions.com>
2020-12-06 14:44:56 -0500

    weighted markov

--- Week 13 - Text/sketch3/sketch.js
+++ Week 13 - Text/sketch3/sketch.js
@@ -1,45 +1,55 @@
-
-function setup() {
-    createCanvas(700, 700)   
-}
-
-
-
-// function draw() {
-
-// }
-
-function keyPressed() {
-    if (key == "a") {
-      save(frameCount + ".png");
-    }
-}
-
-
-
 var firstWords = [];
-const model = generateModel(bill_of_rights);
-const model2 = generateModel2([{text: bill_of_rights, source: 'bor', weight: 0.6}, {text: seuss, source: 'seuss', weight: 0.4}]);
 
-const output_text = generateText("Congress", model);
-const txt = generateText2(model2);
+var weightedSources = [];
+var sourceWeights = {
+  'kanye': 5,
+  'drake': 0
+};
+function updateWeightedSources(){
+  wtTotal = 0;
+  weightedSources = [];
+  for (src in sourceWeights){
+    wt = sourceWeights[src];
+    for (var i=0; i<wt; i++){
+      weightedSources.push(src);
+    }
+  } 
+}
+updateWeightedSources();
+
+function cleanText(str){
+  str = str.replace(/(?:\r\n|\r|\n)/g, '. ')
+  str = str.replace(' . ', '');
+  return str;
+}
 
 
-document.body.innerHTML = output_text;
-// console.log(output_text);
+ 
+Promise.all([
+  fetch("kanye.txt").then(r => r.text()),
+  fetch("drake.txt").then(r => r.text()),
+]).then(([kanye, drake]) => {
+
+  kanye = cleanText(kanye);
+  drake = cleanText(drake);
+  var model = generateModel([{text: kanye, source: 'kanye'}, {text: drake, source: 'drake'}]);
+  var txt = generateText(model);
+
+  console.log(generateText(model), ' - ', generateText(model),generateText(model), ' - ', generateText(model),generateText(model), ' - ', generateText(model))
+
+  document.body.innerHTML = txt;
+
+});
 
 
-
-function generateModel2(input_arr) {
+function generateModel(input_arr) {
 
   //first word either has ton of links or is well, first word
 
-  //word, src, weighmodet, nextWorsd
   var words = [];
   input_arr.forEach(srcobj => {
 
     var src = srcobj.source;
-    
     var weight = src.weight;
 
     //generate first words
@@ -47,16 +57,17 @@ function generateModel2(input_arr) {
     sentences.forEach(s => {
       sArray = s.split(' ');
       firstWords.push(sArray[0]);
-      sArray.forEach(word => {
-        wordObj = {
-          word: word,
-          src: src,
-          firstWordInd: (word == sArray[0]) ? 1: 0,
-          weight: weight
-        };
-        words.push(wordObj);
-      });//end sentence
-    }); //end sentences
+    }); 
+
+    //generate word + source obj
+    var srcwords = srcobj.text.split(' ');
+    srcwords.forEach(word => {
+      words.push({
+        word: word,
+        source: src
+      })
+    });
+
   });
 
   var model = {};
@@ -75,8 +86,7 @@ function generateModel2(input_arr) {
       // add the next word to the possibilities for target_word
       model[target_word].push({
         word: next_word,
-        firstWordInd: nextwordObj.firstWordInd,
-        src: nextwordObj.src 
+        source: nextwordObj.source 
       });
   }
 
@@ -84,9 +94,13 @@ function generateModel2(input_arr) {
 }
 
 
-function generateText2(model) {
+function generateText(m) {
 
   var first_word = firstWords[Math.floor(Math.random() * firstWords.length)];
+
+  var first_word =  "Get"
+
+  //better off getting something capitalized
 
   //start with the word passed in
   let output_text = first_word;
@@ -94,17 +108,29 @@ function generateText2(model) {
   
   for (let i = 0; i < 120; i++) {
       // choose the next word by sampling from options in the model
-      current_word = sample(model[current_word]).word;
+      var source = sample(weightedSources);
+      current_word = sample(m[current_word]).word;
+
+      current_word = weightedSample(source, m[current_word]);
+     
+      if (!current_word ){
+        break;
+      } else {
+        current_word = current_word.word;
+      }
+  
 
       // append word to output
       output_text += " ";
-      output_text += current_word;
+      output_text += current_word.toLowerCase();
 
       // if we get to a word that ends with "." we are done.
       const last_character = current_word.substr(current_word.length - 1);
       if (last_character === ".") {
           break;
       }
+
+      
   }
   return output_text;
 
@@ -115,56 +141,9 @@ function sample(array) {
   return array[index];
 }
 
-//pick a first wordo n its own
-
-
-
-
-
-
-
-function generateModel(input_text) {
-  const words = input_text.split(" ");
-  const model = {};
-  
-
-  // loop through all the words except the last one.
-  for (let i = 0; i < words.length - 1; i++) {
-      const target_word = words[i];
-      const next_word = words[i + 1];
-
-      // if the model doesn't contain the target word, add it.
-      if (!model[target_word]) {
-          model[target_word] = [];
-      }
-
-      // add the next word to the possibilities for target_word
-      model[target_word].push(next_word);
-  }
-
-  return model;
-}
-
-
-function generateText(first_word, model) {
-    // start with the word passed in
-    let output_text = first_word;
-    let current_word = first_word;
-    for (let i = 0; i < 120; i++) {
-        // choose the next word by sampling from options in the model
-        current_word = sample(model[current_word]);
-
-        // append word to output
-        output_text += " ";
-        output_text += current_word;
-
-        // if we get to a word that ends with "." we are done.
-        const last_character = current_word.substr(current_word.length - 1);
-        if (last_character === ".") {
-            break;
-        }
-    }
-    return output_text;
-}
-
+function weightedSample(source, array){
+  var array = array.filter(d=> d.source == source);
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
 
